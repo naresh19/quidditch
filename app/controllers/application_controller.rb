2a2c177c59c5945415cde73c78b6691b12746df6
naresh <naresh.sherwal@infibeam.net>
2015-06-09 15:41:16 +0530

    error added in appn cont

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,48 @@ class ApplicationController < ActionController::Base
       ActiveRecord::Base.transaction do
         yield
       end
+    rescue ActiveRecord::RecordNotFound => e
+      message="Your query fetched no results. "
+      flash.now[:error]= message
+      @error = message
+      log_error e
+      render '/home/index'
+    rescue ActiveRecord::RecordNotUnique => e
+      message="Duplicate Data entered! "
+      flash.now[:error] = message
+      @error = message
+      log_error e
+      render '/home/index'
+    rescue ActiveRecord::ActiveRecordError => e
+      message="Record Not Found : "
+      flash.now[:error]= message
+      @error = message
+      log_error e
+      render '/home/index'
+    rescue NoMethodError => e
+      message="Attempt to access a page that does not exist."
+      flash.now[:error]= message
+      @error = message
+      log_error e
+      render '/home/index'
+    rescue ActionView::Template::Error => e
+      message="Some error occurred while loading the page."
+      flash.now[:error]= message
+      @error = message
+      log_error e
+      render '/home/index'
+    rescue StateMachine::InvalidTransition => e
+      message="Record already modified by some user. kindly refresh."
+      flash.now[:error]= message
+      @error = message
+      log_error e
+      render '/home/index'
+    rescue Exception => e
+      message = "Technical Error! "
+      @error = message
+      flash.now[:error] = message
+      log_error e
+      render '/home/index'
     end
   end
 end
